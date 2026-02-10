fa717e314dc1e6036552e62aec9eda429af9855e
GerardoCL31 <gerardotrajano@gmail.com>
2026-02-10 13:52:23 +0100

    first commit

---
+++ src/main/java/com/ejemplo/tresenraya/TresEnRayaApplication.java
@@ -0,0 +1,12 @@
+package com.ejemplo.tresenraya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TresEnRayaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TresEnRayaApplication.class, args);
+  }
+}
---
+++ src/main/java/com/ejemplo/tresenraya/WebController.java
@@ -0,0 +1,13 @@
+package com.ejemplo.tresenraya;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+}
